2939abfb3c3b23050e8dc0c42d3c898c6eab25a3
ADEWALE MOSES OLAOLUWA <adewalem88@gmail.com>
2025-05-30 12:39:12 +0100

    In Progress

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
-const Home = () => {
+const SignUp = () => {
     
   return (
     
@@ -106,4 +106,4 @@ const Home = () => {
     
   
 
-export default Home
+export default SignUp
